ff1d1ddf1735f191ec1f64651a32cda7619d9cfc
Jiyul Lee <skyrider1811@gmail.com>
2025-12-14 21:03:42 +0900

    :tada: start project

---
+++ src/main/java/com/willu/buyitornot/BuyitornotApplication.java
@@ -0,0 +1,13 @@
+package com.willu.buyitornot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuyitornotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuyitornotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/willu/buyitornot/BuyitornotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.willu.buyitornot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuyitornotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
